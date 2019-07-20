dc7233922ced8eccd2f1d6ce2926f2fde97c1a60
Tim Beswick <contact.tim.here@gmail.com>
2019-07-20 19:49:19 +0100

    Update ts plugin version to 23

--- teamspeak-manager/Plugin.cpp
+++ teamspeak-manager/Plugin.cpp
@@ -11,7 +11,7 @@
 #define _strcpy(dest, destSize, src) { strncpy(dest, src, destSize-1); (dest)[destSize-1] = '\0'; }
 #endif
 
-#define PLUGIN_API_VERSION 22
+#define PLUGIN_API_VERSION 23
 
 #define PATH_BUFSIZE 512
 #define COMMAND_BUFSIZE 128
